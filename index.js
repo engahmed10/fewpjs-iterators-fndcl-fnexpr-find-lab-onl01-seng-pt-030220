8e04c5f3f48525e77df6f3cc5772d6086f296bf4
AHMED AFANDY <eng.computer1@yahoo.com>
2020-09-16 19:47:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 
 const superbowlWin =(record) => {
+<<<<<<< HEAD
   const response= record.find(function(winner) {
+=======
+  const re= record.find(function(winner) {
+>>>>>>> 8674e08c2692a0fe51993f85482c45d368b41530
      return winner.result === "W"
 
   } )
 
 
+<<<<<<< HEAD
     return  (response === undefined) ? undefined : response.year
+=======
+    const aa = (re === undifined) ? undefind : re.year
+>>>>>>> 8674e08c2692a0fe51993f85482c45d368b41530
 
 
 }
+
